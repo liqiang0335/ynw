1ea101be5764ef52a635222dc8900025568897cf
liqiang0335@qq.com <mm020318@2>
2018-05-17 18:56:51 +0800

    20180517185651

--- build/middleware/publicPath.js
+++
@@ -1,9 +0,0 @@
-module.exports = context => option => {
-  const { publicPath, envPublicPath, isDev } = context;
-  if (isDev) {
-    option.output.publicPath = envPublicPath || publicPath;
-  } else {
-    option.output.publicPath = publicPath;
-  }
-  return option;
-};
