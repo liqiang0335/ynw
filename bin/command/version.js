7b09b4d2f13767309bb8122330d4ba4b50ef9d80
liqiang0335@qq.com <liqiang0335@gmail.com>
2018-05-18 23:54:05 +0800

    20180518235404

--- bin/command/version.js
+++ bin/command/version.js
@@ -5,5 +5,5 @@ module.exports = context => {
   const pack = path.join(__dirname, "../../package.json");
   const config = require(pack);
   const { version } = config;
-  console.log(`>> version : ${version}`.green);
+  console.log(`>> version : ${version}`);
 };
