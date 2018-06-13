dc1cb2df425d45c2219501b7ecf4a7da8bf24590
liqiang0335@qq.com <mm020318@2>
2018-06-13 19:21:08 +0800

    add mock api

--- bin/command/build/middleware/devServer.js
+++ bin/command/build/middleware/devServer.js
@@ -1,9 +1,18 @@
 const path = require("path");
 const load = require("./load");
 const webpack = load("webpack");
+const fs = require("fs");
 
 module.exports = context => option => {
-  const { hot, port, fileName, absolutePath, projectPath, devServer } = context;
+  const {
+    hot,
+    port,
+    fileName,
+    absolutePath,
+    projectPath,
+    devServer,
+    fns
+  } = context;
   if (!context.hot) return option;
 
   option.entry = {
@@ -24,6 +33,14 @@ module.exports = context => option => {
     },
     devServer
   );
+
+  //add mock.api
+  const mockFile = path.join(projectPath, "mock.js");
+  if (fs.existsSync(mockFile)) {
+    const routers = require(mockFile);
+    option.devServer.before = routers;
+  }
+
   option.plugins.push(new webpack.HotModuleReplacementPlugin());
   option.plugins.push(new webpack.NamedModulesPlugin());
   return option;
