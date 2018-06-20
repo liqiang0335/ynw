7aed6d2910e59154464057c3af343b3f61c52629
liqiang0335@qq.com <mm020318@2>
2018-06-20 17:31:19 +0800

    20180620173118

--- bin/index.js
+++ bin/index.js
@@ -7,21 +7,11 @@ const commands = fns.getParams(process.argv);
 const cwd = process.cwd();
 const context = { fns, cwd, env: "dev", ...commands };
 
-const shortMap = {
-  v: "version",
-  b: "build",
-  e: "env",
-  i: "init"
-};
-
 (async function() {
   const folder = path.join(__dirname, "./command");
   const docs = await fns.readdir(folder).catch(err => console.log(err));
   const files = docs.map(doc => doc.replace(/\.[a-z]+$/, ""));
   Object.keys(commands).forEach(key => {
-    if (shortMap[key]) {
-      key = shortMap[key];
-    }
     if (files.indexOf(key) < 0) return;
     const command = require(`./command/${key}`);
     command(context);
