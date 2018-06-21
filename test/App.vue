2ad146d02e7b5a708d0d89e6c874a888fe367d2f
liqiang0335@qq.com <mm020318@2>
2018-06-21 15:34:14 +0800

    20180621153413

--- test/App.vue
+++ test/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <yn-scroll></yn-scroll>
+    <yn-header></yn-header>
+    <div class="line"></div>
+    <yn-list></yn-list>
   </div>
 </template>
+<style lang="scss" scoped>
+.line {
+  height: 2px solid gray;
+}
+</style>
+
