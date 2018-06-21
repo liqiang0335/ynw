bfdf013fb0d90ebcaa99ed8b6b2b7c38316bf87b
liqiang0335@qq.com <mm020318@2>
2018-06-21 13:41:33 +0800

    20180621134133

--- browser/getScrollPotition.js
+++
@@ -1,9 +0,0 @@
-/**
- * 获取滚动位置
- */
-const getScrollPosition = (el = window) => ({
-  x: el.pageXOffset !== undefined ? el.pageXOffset : el.scrollLeft,
-  y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrollTop
-});
-
-module.exports = getScrollPosition;
