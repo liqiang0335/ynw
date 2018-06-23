c62a5886f96e9c963d3ee4ee54a911fedfdd1988
liqiang0335@qq.com <liqiang0335@gmail.com>
2018-06-23 22:22:19 +0800

    20180623222218

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-var a = "Hello";
+const o = {
+  a: 1
+};
+
+const trap_o = trap(o);
+
+trap_o.b = 2;
+console.log(yntrap_o);
+
+function trap(obj) {
+  return new Proxy(obj, {
+    set(target, key, value) {
+      console.log("----");
+      return Reflect.set(target, key, value);
+    }
+  });
+}
