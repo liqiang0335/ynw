46d0cebca6122694c7e1ab5fa8faa6943c36485e
liqiang0335@qq.com <mm020318@2>
2018-06-29 16:03:20 +0800

    20180629160320

--- util/onpress.js
+++ util/onpress.js
@@ -1,14 +1,64 @@
-let init = false
-if (process.env.NODE_ENV !== "production") {
-  const handler = {
-    96:()=>
-    105: () => 
-    57: () => 
-  };
+/**
+ * @param handlers: Object 处理函数
+ *
+ * 使用方法
+ * import onPress from 'ynw/util/onPress
+ *  onPress({
+ *    KeyA:()=>console.log("you press A"),
+ *    Num2:()=>console.log("you press 2")
+ * })
+ */
+export default handlers => {
+  const isDev = process.env.NODE_ENV !== "production";
+  document.addEventListener(
+    "keyup",
+    e => {
+      const { altKey, ctrlKey, keyCode } = e;
+      if (!(ctrlKey && altKey)) return;
+      const key = CodeToKey[keyCode];
+      if (handlers[key]) {
+        handlers[key]();
+      }
+    },
+    false
+  );
+};
 
-  document.addEventListener("keyup", e => {
-    const { altKey,ctrlKey, keyCode } = e;
-    if (!(ctrlKey && altKey && shiftKey)) return;
-    handler[keyCode] && handler[keyCode]();
-  });
-}
+const CodeToKey = {
+  48: "Num0",
+  49: "Num1",
+  50: "Num2",
+  51: "Num3",
+  52: "Num4",
+  53: "Num5",
+  54: "Num6",
+  55: "Num7",
+  56: "Num8",
+  57: "Num9",
+  65: "KeyA",
+  66: "KeyB",
+  67: "KeyC",
+  68: "KeyD",
+  69: "KeyE",
+  70: "KeyF",
+  71: "KeyG",
+  72: "KeyH",
+  73: "KeyI",
+  74: "KeyJ",
+  75: "KeyK",
+  76: "KeyL",
+  77: "KeyM",
+  78: "KeyN",
+  79: "KeyO",
+  80: "KeyP",
+  81: "KeyQ",
+  82: "KeyR",
+  83: "KeyS",
+  84: "KeyT",
+  85: "KeyU",
+  86: "KeyV",
+  87: "KeyW",
+  88: "KeyX",
+  89: "KeyY",
+  90: "KeyZ"
+};
