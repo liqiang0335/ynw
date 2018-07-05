bd5dcc38f3d74c265d393c2ca4867ab4f0496b96
liqiang0335@qq.com <mm020318@2>
2018-07-05 17:49:59 +0800

    20180705174959

--- bin/command/build/middleware/bablerc.js
+++ bin/command/build/middleware/bablerc.js
@@ -1,11 +1,23 @@
 const path = require("path");
 const fs = require("fs");
 
+const detectReact = context => {
+  const { absolutePath, cwd } = context;
+  const filePath = absolutePath + ".js";
+  const entry = fs.readFileSync(filePath, "utf-8");
+  const isReact = !!entry.match(/import.+from\s+"react"/);
+  const babel = path.join(cwd, ".babelrc");
+  const bcContent = fs.readFileSync(babel, "utf-8");
+  const valid = !!bcContent.match(/react/);
+  if (isReact) {
+    if (valid) return;
+  }
+};
+
 module.exports = context => option => {
-  const { browsers } = context;
+  const { browsers, cwd } = context;
   const has = Array.isArray(browsers) && browsers.length > 0;
   const value = has ? JSON.stringify(browsers) : `["ie >= 9"]`;
-  const cwd = process.cwd();
   const file = path.join(cwd, ".babelrc");
   const doc = fs.readFileSync(file, "utf-8");
   const content = doc.replace(/"browsers.+?\]/, `"browsers": ${value}`);
