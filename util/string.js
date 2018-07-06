ff4027329df6879f93c010886f0e8b8b76772b82
liqiang0335@qq.com <mm020318@2>
2018-07-06 09:14:14 +0800

    全角转半角函数

---
+++ util/string.js
@@ -0,0 +1,14 @@
+/**
+ * 全角转半角字符
+ */
+export function ToCDB(str) {
+  var tmp = "";
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 65248 && str.charCodeAt(i) < 65375) {
+      tmp += String.fromCharCode(str.charCodeAt(i) - 65248);
+    } else {
+      tmp += String.fromCharCode(str.charCodeAt(i));
+    }
+  }
+  return tmp;
+}
