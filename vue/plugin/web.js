ba038309c6af072dda9ff643624f2fd32f27d4fd
liqiang0335@qq.com <mm020318@2>
2018-07-10 13:46:00 +0800

    replace fail to error

--- vue/plugin/web.js
+++ vue/plugin/web.js
@@ -3,6 +3,7 @@ import { Message } from "element-ui";
 export default {
   install(Vue, option) {
     Vue.prototype.$error = message => Message({ type: "error", message });
+    Vue.prototype.$fail = message => Message({ type: "error", message });
     Vue.prototype.$success = message => Message({ type: "success", message });
     Vue.prototype.$warning = message => Message({ type: "warning", message });
 
