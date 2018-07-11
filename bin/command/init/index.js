a79c9b5d1541ea2176d12451b0070e52934dd2e0
liqiang0335@qq.com <mm020318@2>
2018-07-11 13:43:29 +0800

    20180711134328

--- bin/command/init/index.js
+++ bin/command/init/index.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const load = require("../build/middleware/load");
-load("colors");
 
 const getDir = name => path.join(__dirname, name);
 
@@ -13,12 +12,12 @@ const folders = {
 const write = async ({ fns, cwd }, { filePath, fileName }) => {
   const target = path.join(cwd, fileName);
   if (await fns.exists(target)) {
-    console.log(`>> [ ${fileName} ] exists`.yellow);
+    console.log(`>> [ ${fileName} ] exists`);
     return;
   }
   const content = await fns.readFile(filePath, "utf-8");
   await fns.writeFile(target, content, "utf-8");
-  console.log(`>> write [ ${fileName} ] done`.green);
+  console.log(`>> write [ ${fileName} ] done`);
 };
 
 const extraHandler = async context => {
