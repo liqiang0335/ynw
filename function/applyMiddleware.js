ed1ae9c767cc21787833867765c128b501b1f384
liqiang0335@qq.com <mm020318@2>
2018-07-12 13:13:27 +0800

    first commit

---
+++ function/applyMiddleware.js
@@ -0,0 +1,15 @@
+const composeLeft = (...fn) => {
+  return fn.reduce((a, b) => (...args) => a(b(...args)));
+};
+
+const composeRight = (...fn) => {
+  return fn.reduce((a, b) => (...args) => b(a(...args)));
+};
+
+const applyMiddleware = (api, middlewares, dir = "right") => {
+  const chain = middlewares.map(item => item(api));
+  const fn = dir == "right" ? composeRight : composeLeft;
+  return fn(...chain);
+};
+
+module.exports = applyMiddleware;
