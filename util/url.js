00615a85a19a335099696e55956b6750da05f0a6
liqiang0335@qq.com <mm020318@2>
2018-07-23 14:51:17 +0800

    20180723145116

---
+++ util/url.js
@@ -0,0 +1,21 @@
+const getURLParams = url =>
+  (url.match(/([^?=&]+)(=([^&]*))/g) || []).reduce(
+    (a, v) => (
+      (a[v.slice(0, v.indexOf("="))] = v.slice(v.indexOf("=") + 1)), a
+    ),
+    {}
+  );
+
+const isAbsoluteURL = str => /^[a-z][a-z0-9+.-]*:/.test(str);
+
+const URLJoin = (...args) =>
+  args
+    .join("/")
+    .replace(/[\/]+/g, "/")
+    .replace(/^(.+):\//, "$1://")
+    .replace(/^file:/, "file:/")
+    .replace(/\/(\?|&|#[^!])/g, "$1")
+    .replace(/\?/g, "&")
+    .replace("&", "?");
+
+module.exports = { getURLParams, isAbsoluteURL, URLJoin };
