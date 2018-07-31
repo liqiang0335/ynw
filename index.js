3a0cd76276c7e6f37e1233316e8f23d7aa463f89
liqiang0335@qq.com <mm020318@2>
2018-07-31 13:40:08 +0800

    20180731134007

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const { request, attach } = require("./lib/axios");
-const { setter, logger } = require("./lib/vuex");
-
-module.exports = {
-  request,
-  attach,
-  setter,
-  logger
-};
+console.log("from ynw ....");
