38e877fd0157f3da0a6798a9a3681a7b51de26c0
liqiang0335@qq.com <liqiang0335@gmail.com>
2018-09-25 00:12:10 +0800

    bug fixed

--- algorithms/search/binarySearch.js
+++ algorithms/search/binarySearch.js
@@ -1,20 +1,26 @@
+import Comparator from "../util/Comparator";
+
 /**
  * 二分查找
  *
  * @param {*[]} arr 已经排好序的数组
  * @param {*} seek 要查找的元素
+ * @param {function(a, b)} [callback] 比较函数
  * @return {number}
  */
-export default function binarySearch(arr, seek) {
+export default function binarySearch(arr, seek, callback) {
+  const comparator = new Comparator(callback);
   let start = 0;
   let end = arr.length - 1;
 
   while (start <= end) {
     const middle = start + Math.floor((end - start) / 2);
-    if (arr[middle] == seek) {
+
+    if (comparator.equal(arr[middle], seek)) {
       return middle;
     }
-    if (arr[middle] < seek) {
+
+    if (comparator.lessThan(arr[middle], seek)) {
       start = middle + 1;
     } else {
       end = middle - 1;
