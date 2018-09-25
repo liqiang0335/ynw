1186e9b47f048aa6c8d2eeda311d48cba77b836f
liqiang <liqiang0335@qq.com>
2018-09-25 18:00:21 +0800

    update

---
+++ dataStructures/HashTable.js
@@ -0,0 +1,63 @@
+const LinkedList = require("./LinkedList");
+
+// The bigger size the less collisions
+const defaultHashTableSize = 32;
+
+class HashTable {
+  constructor(hashTableSize = defaultHashTableSize) {
+    this.buckets = Array(hashTableSize)
+      .fill(null)
+      .map(() => new LinkedList());
+
+    // Just to keep track of all actual keys in a fast way.
+    this.keys = {};
+  }
+
+  //计算哈希值
+  hash(key) {
+    const hash = Array.from(key).reduce((acc, cur) => {
+      return acc + cur.charCodeAt(0);
+    }, 0);
+    return hash % this.buckets.length;
+  }
+
+  set(key, value) {
+    const hash = this.hash(key);
+    this.keys[key] = hash;
+    const bucket = this.buckets[hash];
+    const node = bucket.find({ callback: item => item.key === key });
+    if (!node) {
+      bucket.append({ key, value }); //Insert new node.
+    } else {
+      node.value.value = value; //Update value of existing node
+    }
+  }
+
+  delete(key) {
+    const hash = this.hash(key);
+    delete this.keys[hash];
+    const bucket = this.buckets[hash];
+    const node = bucket.find({ callback: item => item.key === key });
+    if (node) {
+      return bucket.delete(node.value);
+    }
+    return null;
+  }
+
+  get(key) {
+    const hash = this.hash(key);
+    const bucket = this.buckets[hash];
+    const node = bucket.find({ callback: item => item.key === key });
+    return node ? node.value.value : undefined;
+  }
+
+  has(key) {
+    return Object.hasOwnProperty.call(this.keys, key);
+  }
+
+  getKeys() {
+    return Object.keys(this.keys);
+  }
+}
+
+module.exports = HashTable;
---
+++ dataStructures/tree/BinaryTreeNode.js
@@ -0,0 +1,143 @@
+const HashTable = require("../HashTable");
+
+class BinaryTreeNode {
+  constructor(value = null) {
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.value = null;
+
+    this.meta = new HashTable();
+    this.equal = function(a, b) {
+      return a === b;
+    };
+  }
+
+  get leftHeight() {
+    if (!this.left) {
+      return 0;
+    }
+
+    return this.left.height + 1;
+  }
+
+  get rightHeight() {
+    if (!this.right) {
+      return 0;
+    }
+    return this.right.height + 1;
+  }
+
+  get height() {
+    return Math.max(this.leftHeight, this.rightHeight);
+  }
+
+  get balanceFactor() {
+    return this.leftHeight - this.rightHeight;
+  }
+
+  get uncle() {
+    if (!this.parent) {
+      return undefined;
+    }
+
+    if (!this.parent.parent) {
+      return undefined;
+    }
+
+    if (!this.parent.parent.left || !this.parent.parent.right) {
+      return undefined;
+    }
+
+    if (this.equal(this.parent, this.parent.parent.left)) {
+      return this.parent.parent.right;
+    }
+
+    return this.parent.parent.left;
+  }
+
+  setValue(value) {
+    this.value = value;
+    return this;
+  }
+
+  setLeft(node) {
+    if (this.left) {
+      this.left.parent = null;
+    }
+
+    this.left = node;
+
+    if (this.left) {
+      this.left.parent = this;
+    }
+
+    return this;
+  }
+
+  setRight(node) {
+    if (this.right) {
+      this.right.parent = null;
+    }
+
+    this.right = node;
+    if (node) {
+      this.right.parent = this;
+    }
+
+    return this;
+  }
+
+  removeChild(node) {
+    if (this.left && this.equal(this.left, node)) {
+      this.left = null;
+      return true;
+    }
+
+    if (this.right && this.equal(this.right, node)) {
+      this.right = null;
+      return true;
+    }
+
+    return false;
+  }
+
+  replaceChild(before, after) {
+    if (!before || !after) {
+      return false;
+    }
+    if (this.left && this.equal(this.left, before)) {
+      this.left = after;
+      return true;
+    }
+    if (this.right && this.equal(this.right, before)) {
+      this.right = after;
+      return true;
+    }
+
+    return false;
+  }
+
+  static copyNode(source, target) {
+    target.setValue(source.value);
+    target.setLeft(source.left);
+    target.setRight(source.right);
+  }
+
+  traverseInOrder() {
+    const traverse = [];
+    if (this.left) {
+      traverse = traverse.concat(this.left.traverseInOrder());
+    }
+    traverse.push(this.value); //add root
+    if (this.right) {
+      traverse = traverse.concat(this.right.traverseInOrder());
+    }
+
+    return traverse;
+  }
+
+  toString() {
+    return this.traverseInOrder().toString();
+  }
+}
