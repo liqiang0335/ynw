e97319260f485c7da9d1d0957b94b96f6dcd376e
liqiang <liqiang0335@qq.com>
2018-09-26 18:54:23 +0800

    update

--- function/nextChain.js
+++ function/nextChain.js
@@ -1,12 +1,12 @@
 /**
  * 依次执行队列中的同步或异步函数
  */
-const nextChain = fns => {
+const nextChain = (...args) => fns => {
   let cur = 0;
   const lastIndex = fns.length - 1;
   const next = () => {
     const _next = cur == lastIndex ? f => f : next;
-    fns[cur++](_next);
+    fns[cur++].apply(null, [...args, _next]);
   };
   next();
 };
--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-const LinkedList = require("./data-structures/linkedList");
-const list = new LinkedList();
-list.append(1);
-console.log(list);
+const nextChain = (...args) => fns => {
+  let cur = 0;
+  const lastIndex = fns.length - 1;
+  const next = () => {
+    const _next = cur == lastIndex ? f => f : next;
+    fns[cur++].apply(null, [...args, _next]);
+  };
+  next();
+};
+
+const chain = nextChain();
+
+const f1 = next => {
+  console.log("f1");
+  next();
+};
+const f2 = next => {
+  console.log("f2");
+  next();
+};
+
+chain([f1, f2]);
