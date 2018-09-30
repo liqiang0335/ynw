213a2fa43f29424489f869601e41f696f9f4092c
liqiang <liqiang0335@qq.com>
2018-09-30 14:58:43 +0800

    upate

--- bin/command/build/output/append.js
+++ bin/command/build/output/append.js
@@ -1,16 +1,14 @@
 const fs = require("fs");
 
 module.exports = context => files => {
-  const { projectPath, hot, fileName, buildTime, usePWA, env } = context;
+  const { projectPath, hot, fileName, buildTime, env } = context;
   if (hot) return files;
 
   const file = `${projectPath}/dist/${fileName}.bundle.js`;
   const content = fs.readFileSync(file, "utf-8");
-  const pwa = usePWA || "";
   const extra = [];
   extra.push(`window.WEBPACK_BUILD_TIME = "${buildTime}";`);
-  extra.push(`window.WEBPACK_PWA = "${pwa}";`);
   extra.push(`window.WEBPACK_ENV = "${env}";`);
-  const target = content + extra.join("");
+  const target = extra.join("") + content;
   fs.writeFileSync(file, target);
 };
--- index.js
+++ index.js
@@ -30,16 +30,6 @@ const handler = {
         `%c BUILD TIME : ${time}`,
         "font-weight:bold;background:green;color:white;padding:2px 4px;"
       );
-      //enable pwa
-      if (window.WEBPACK_PWA && "serviceWorker" in navigator) {
-        setTimeout(() => {
-          navigator.serviceWorker
-            .register("/sw.js")
-            .catch(err =>
-              console.log(`serviceWorker registration failed : ${err}`)
-            );
-        }, 1000);
-      }
     });
   }
 };
