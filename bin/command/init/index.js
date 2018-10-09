5c44dc49211522eea1939f08c3c96cd568359f88
liqiang <liqiang0335@qq.com>
2018-10-09 17:00:19 +0800

    remove react build

--- bin/command/init/index.js
+++ bin/command/init/index.js
@@ -1,17 +1,11 @@
 const path = require("path");
 const load = require("../build/middleware/load");
 
-const getDir = name => path.join(__dirname, name);
-
-const folders = {
-  common: getDir("common"),
-  vue: getDir("vue"),
-  react: getDir("react")
-};
+const overrides = new Set([".babelrc", ".eslintrc.js"]);
 
 const write = async ({ fns, cwd }, { filePath, fileName }) => {
   const target = path.join(cwd, fileName);
-  if (await fns.exists(target)) {
+  if (!overrides.has(fileName) && (await fns.exists(target))) {
     console.log(`>> [ ${fileName} ] exists`);
     return;
   }
@@ -20,41 +14,15 @@ const write = async ({ fns, cwd }, { filePath, fileName }) => {
   console.log(`>> write [ ${fileName} ] done`);
 };
 
-const extraHandler = async context => {
-  const { fns } = context;
-  const folder = getDir("extra");
-  const files = await fns.readdir(folder, "utf-8");
-  files.forEach(fileName => {
-    const command = fileName.replace(/\.[a-z]+$/, "");
-    if (context[command]) {
-      const filePath = path.join(folder, fileName);
-      write(context, { filePath, fileName });
-    }
-  });
-};
-
-const commandHandler = context => {
-  const { fns, init } = context;
-  if (!folders[init]) {
-    return;
-  }
-  createAllFiles(folders.common);
-  createAllFiles(folders[init]);
-
-  async function createAllFiles(folder) {
-    const files = await fns.readdir(folder, "utf-8");
-    files.forEach(fileName => {
-      const filePath = path.join(folder, fileName);
-      write(context, { filePath, fileName });
-    });
-  }
-};
-
 module.exports = async context => {
   const { fns, init } = context;
   if (!init) {
     return;
   }
-  const pipes = [extraHandler, commandHandler];
-  pipes.forEach(fn => fn(context));
+  const source = path.join(__dirname, "./source");
+  const files = await fns.readdir(source, "utf-8");
+  files.forEach(fileName => {
+    const filePath = path.join(source, fileName);
+    write(context, { filePath, fileName });
+  });
 };
