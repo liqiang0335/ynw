2957f13106b6f658d61c86a372b07c6c75c00c4f
liqiang <liqiang0335@qq.com>
2018-10-10 09:06:32 +0800

    update

--- bin/command/version.js
+++ bin/command/version.js
@@ -2,7 +2,7 @@ const path = require("path");
 const load = require("./build/middleware/load");
 
 module.exports = context => {
-  const pack = path.join(__dirname, "../package.json");
+  const pack = path.join(__dirname, "../../package.json");
   const config = require(pack);
   const { version } = config;
   console.log(`>> version : ${version}`);
