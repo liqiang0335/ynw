86a399d44cdb3e2d23a08b4ca87f47420585e8ab
liqiang <liqiang0335@qq.com>
2019-05-05 14:54:18 +0800

    Bug Fixed

--- countDown.js
+++ countDown.js
@@ -1,17 +1,16 @@
 export default function counterDown(callback, duration = 60) {
-  return new Promise(resolve => {
-    let timerId = null;
-    let counter = duration;
-    timerId = setInterval(() => {
-      if (counter === 0) {
-        clearInterval(timerId);
-        timerId = null;
-        counter = duration;
-        resolve();
-        return;
-      }
+  let timerId = null;
+  let counter = duration;
+  timerId = setInterval(() => {
+    if (counter === 0) {
+      clearInterval(timerId);
+      timerId = null;
+      counter = duration;
+      resolve();
+      return;
+    } else {
       callback(counter);
       counter--;
-    }, 1000);
-  });
+    }
+  }, 1000);
 }
