729a77660099989cbb3cf03153f21e486ede736a
liqiang <liqiang0335@qq.com>
2019-05-29 11:18:54 +0800

    add

---
+++ formatDuration.js
@@ -0,0 +1,12 @@
+const formatDuration = ms => {
+  if (ms < 0) ms = -ms;
+  return {
+    day: Math.floor(ms / 86400000),
+    hour: Math.floor(ms / 3600000) % 24,
+    minute: Math.floor(ms / 60000) % 60,
+    second: Math.floor(ms / 1000) % 60,
+    millisecond: Math.floor(ms) % 1000
+  };
+};
+
+module.exports = formatDuration;
