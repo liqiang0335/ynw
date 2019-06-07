5e9288c0f7ab9ef6977ceee02b710474a1f24e51
liqiang0335@qq.com <liqiang0335@gmail.com>
2019-06-07 22:01:38 +0800

    update

--- react/index.js
+++
@@ -1,7 +0,0 @@
-export { default as useCountdown } from "./useCountdown";
-export { default as useInterval } from "./useInterval";
-export { default as useKeyboardJs } from "./useKeyboardJs";
-export { default as useObservable } from "./useObservable";
-export { default as useScroll } from "./useScroll";
-export { default as useService } from "./useService";
-export { default as useWindowSize } from "./useWindowSize";
