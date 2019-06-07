6b84de89ade6218ef57ed5164a483bad1e7dd28d
liqiang <liqiang0335@qq.com>
2019-06-07 15:55:58 +0800

    flatten

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-export { default as HashTable } from "./algorithm/HashTable";
-export { default as LinkedList } from "./algorithm/LinkedList";
-export { default as Queue } from "./algorithm/Queue";
-export { default as Stack } from "./algorithm/Stack";
+var createElement = function() {
+  var div = document.createElement("div");
+  var style = {
+    background: "#0baa23",
+    color: "white",
+    padding: "2px 4px",
+    display: "inline-block",
+    position: "fixed",
+    right: "0",
+    bottom: "0",
+    fontSize: "12px"
+  };
+  for (var key in style) {
+    div.style[key] = style[key];
+  }
+  return div;
+};
 
-// Browser
-export { default as copyToClipboard } from "./browser/copyToClipboard";
-export { default as detectBrowser } from "./browser/detectBrowser";
-export { default as detectDeviceType } from "./browser/detectDeviceType";
-export { default as elementInViewport } from "./browser/elementInViewport";
-export { default as getUrlParams } from "./browser/getUrlParams";
-export { default as touch } from "./browser/touch";
+var handler = {
+  development: function() {
+    document.addEventListener(
+      "DOMContentLoaded",
+      function() {
+        var div = createElement();
+        var time = window.WEBPACK_BUILD_TIME;
+        div.textContent = time;
+        document.body.appendChild(div);
+      },
+      false
+    );
+  },
+  production: function() {
+    document.addEventListener(
+      "DOMContentLoaded",
+      function() {
+        var time = window.WEBPACK_BUILD_TIME;
+        console.log(
+          "%c BUILD TIME : " + time,
+          "font-weight:bold;background:green;color:white;padding:2px 4px;"
+        );
+      },
+      false
+    );
+  }
+};
 
-// Chain
-export { default as applyMiddleware } from "./chain/applyMiddleware";
-export { default as compose } from "./chain/compose";
-export { default as composeRight } from "./chain/composeRight";
-export { default as koaChain } from "./chain/koaChain";
-export { default as nextChain } from "./chain/nextChain";
-export { default as promiseChain } from "./chain/promiseChain";
-
-// Function
-export { default as array2Tree } from "./function/array2Tree";
-export { default as counterDown } from "./function/counterDown";
-export { default as createEventHub } from "./function/createEventHub";
-export { default as debounce } from "./function/debounce";
-export { default as extend } from "./function/extend";
-export { default as tree2Array } from "./function/tree2Array";
-
-// React
-export { default as useCountdown } from "./react/useCountdown";
-export { default as useInterval } from "./react/useInterval";
-export { default as useObservable } from "./react/useObservable";
-export { default as useWindowSize } from "./react/useWindowSize";
-export { default as useScroll } from "./react/useScroll";
-
-// Time
-export { default as formatDuration } from "./time/formatDuration";
-
-// Utils
-export { default as chineseNumber } from "./utils/chineseNumber";
-export { default as validateIdCard } from "./utils/validateIdCard";
+handler[process.env.NODE_ENV]();
---
+++ react/index.js
@@ -0,0 +1,7 @@
+export { default as useCountdown } from "./useCountdown";
+export { default as useInterval } from "./useInterval";
+export { default as useKeyboardJs } from "./useKeyboardJs";
+export { default as useObservable } from "./useObservable";
+export { default as useScroll } from "./useScroll";
+export { default as useService } from "./useService";
+export { default as useWindowSize } from "./useWindowSize";
