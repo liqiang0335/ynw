25fce8941e828f87f2ad8d8fa273ad91abee5025
778629322 <778629322@qq.com>
2019-07-10 10:50:26 +0800

    get urlParams   return Object

--- getUrlJoin.js
+++
@@ -1,3 +0,0 @@
-const params = {};
-location.search.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => (params[k] = v));
-export default params;
--- getUrlParams.js
+++ getUrlParams.js
@@ -1,8 +1,6 @@
-export default function getUrlParams(href = window.location.search) {
-  return (encodeURI(href).match(/([\w_-]+)=([^&]+)/gi) || [])
-    .map(item => {
-      const arr = item.split("=");
-      return { [arr[0]]: decodeURI(arr[1]) };
-    })
-    .reduce((prev, cur) => ({ ...prev, ...cur }), {});
-}
+const params = {};
+encodeURI(location.search).replace(
+  /([^?&=]+)=([^&]+)/g,
+  (_, k, v) => (params[k] = decodeURI(v))
+);
+export default params;
