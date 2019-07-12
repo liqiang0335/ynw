d06fe96fe9034d501c653e07d559e232662616af
liqiang0335@qq.com <liqiang0335@gmail.com>
2019-07-13 00:20:54 +0800

    as function

--- getUrlParams.js
+++ getUrlParams.js
@@ -1,6 +1,11 @@
-const params = {};
-encodeURI(location.search).replace(
-  /([^?&=]+)=([^&]+)/g,
-  (_, k, v) => (params[k] = decodeURI(v))
-);
-export default params;
+/**
+ * 通过函数获取参数
+ */
+export default function getUrlParams(href = window.location.search) {
+  const params = {};
+  encodeURI(href).replace(
+    /([^?&=]+)=([^&]+)/g,
+    (_, k, v) => (params[k] = decodeURI(v))
+  );
+  return params;
+}
