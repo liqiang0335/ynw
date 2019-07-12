87a0c37956ebbb8db1d805a8e06176633f2ac951
liqiang <liqiang0335@qq.com>
2019-07-12 17:44:36 +0800

    add

---
+++ fillZero.js
@@ -0,0 +1,17 @@
+import padEnd from "lodash/padEnd";
+
+/**
+ * 补全小数(用0填充)
+ * @param {Number, String} num - 数字或字符
+ * @param {Number} length - 补全长度
+ * @return {String}
+ */
+export default function fillZero(num, length) {
+  if (!/\./.test(num)) {
+    const len = Array.from({ length }, () => 0).join("");
+    return `${num}.${len}`;
+  }
+  return `${num}`.replace(/\.\d+/, match => {
+    return padEnd(match, length + 1, "0");
+  });
+}
