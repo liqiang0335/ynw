f372d6eb36ec37fff7d54e67a5bec7d0d8e28be5
liqiang <liqiang0335@qq.com>
2019-09-04 13:15:15 +0800

    add

--- yngp/goto.js
+++ yngp/goto.js
@@ -18,11 +18,11 @@ export const toWeb = url => {
 
 /**
  * 跳转到股票行情
- * @param {Number} code
- * @param {String} market
- * @param {String} name
+ * @param {Number} code - 股票代码
+ * @param {String} market - 市场代码: 上海=XSHG 深圳=XSHE
+ * @param {String} name - 股票名称
  */
-export const toStock = ({ code, market, name }) => {
+export const toStock = (code, market, name) => {
   const stockCode = `${code}.${market}`;
   const stockName = name;
   if (ios) {
