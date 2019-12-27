988496abc9e5e989cff0f07c7634e24f34f75e5f
liqiang <liqiang0335@qq.com>
2019-12-27 18:26:59 +0800

    add

---
+++ getSubstring.js
@@ -0,0 +1,15 @@
+/**
+ * 短文本
+ * @param {String} str - 文本
+ * @param {Number} length - 长度
+ * @param {Number} suffix - 结尾, 默认..
+ * @return {String}
+ */
+export default function getSubstring(str, length, suffix = "..") {
+  const value = String(str);
+  const len = value.length;
+  if (len >= length) {
+    return value.substring(0, length) + suffix;
+  }
+  return value;
+}
