b8242a8703f1846c87ffcc3c7975255f34d1df97
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-03-16 11:07:11 +0800

    add

---
+++ script/confirmer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Modal } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+/**
+ * 操作确认
+ * @returns {Promise}
+ */
+export default function confirmer(content, title = "操作确认") {
+  return new Promise((resolve, reject) => {
+    Modal.confirm({
+      icon: <ExclamationCircleOutlined style={{ color: "red" }} />,
+      title: title,
+      content: content,
+      async onOk() {
+        resolve();
+      },
+      onCancel() {
+        reject();
+      },
+    });
+  });
+}
--- script/previewImage.js
+++ script/previewImage.js
@@ -1,8 +1,9 @@
 import Viewer from "viewerjs";
 import "viewerjs/dist/viewer.css";
-
 /**
+ * 预览图片
  * @param {String} src - 预览的图片
+ * @param {Object} [option] - viewerjs选项
  */
 export default function previewImage(src, option) {
   let img = new Image();
@@ -19,7 +20,7 @@ export default function previewImage(src, option) {
     fullscreen: true,
     viewed: () => viewer.zoomTo(1),
     hidden: () => viewer.destroy(),
-    ...option
+    ...option,
   });
 
   viewer.show();
