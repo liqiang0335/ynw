4232756ba0bbfeb7c727c62f4946b4831b4d0d09
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-05-20 23:24:36 +0800

    增加缓存

--- antd/FetchMore.js
+++ antd/FetchMore.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from "react";
 import { LoadingOutlined } from "@ant-design/icons";
 import { useDebounceFn } from "ahooks";
 import http from "@script/http";
+const CACHES = {}; // 数据缓存
 
 const initState = {
   data: [],
@@ -18,9 +19,16 @@ const reducer = (state, action) => {
  * @param {Function} ondata - 数据回调
  * @param {Array} [deps] - 变化依赖
  * @param {Number} [size = 10] - 加载数量
+ * @param {String} [cached = ''] - 是否缓存数据, 设置缓存数据的key, 并保证唯一性
  * ----------------------------------------
  */
-export default function FetchMore({ url, ondata, deps, size = 10 }) {
+export default function FetchMore({
+  url,
+  ondata,
+  deps,
+  size = 10,
+  cached = "",
+}) {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const { run } = useDebounceFn(() => fetcher(true), {
@@ -42,11 +50,19 @@ export default function FetchMore({ url, ondata, deps, size = 10 }) {
     const data = isAppend ? state.data.concat(res) : res;
     const hasMore = res.length === size;
     dispatch({ loading: false, data, hasMore });
+    if (cached) {
+      CACHES[cached] = data;
+    }
     ondata(data);
   };
 
   useEffect(() => {
-    fetcher(false);
+    if (cached && CACHES[cached]?.length > 0) {
+      dispatch({ data: CACHES[cached], hasMore: true });
+      ondata(CACHES[cached]);
+    } else {
+      fetcher(false);
+    }
   }, [].concat(deps));
 
   return <Loading onClick={run} state={state}></Loading>;
