45169123d97663efd66c530a0b4c578798124452
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-05-23 21:30:58 +0800

    add

--- antd/ShowMulFile.js
+++ antd/ShowMulFile.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Popover, Button } from "antd";
 import Download from "ynw/antd/Download";
+import { FilePptOutlined } from "@ant-design/icons";
 /**
  * ----------------------------------------
  * 显示多个文件下载
+ * @param {Array} - files - 文件路径列表
+ * @param {Boolean} - [showCount] - 是否显示数量
  * ----------------------------------------
  */
-export default function ShowMulFile({ files }) {
+export default function ShowMulFile({ files, showCount = false }) {
   const values = files || [];
   const len = values.length;
 
@@ -25,7 +28,14 @@ export default function ShowMulFile({ files }) {
   };
   return (
     <Popover title="文件下载" content={Content} trigger="click">
-      <Button type="link">{values.length}</Button>
+      <Button type="link">
+        <FilePptOutlined style={{ fontSize: "16px" }} />
+        {showCount ? (
+          <span style={{ marginLeft: "5px" }}>{values.length}</span>
+        ) : (
+          ""
+        )}
+      </Button>
     </Popover>
   );
 }
