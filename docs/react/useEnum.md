710fa5fed98b784711d7a9b2a1e3146c4646afcc
liqiang0335 <liqiang0335@qq.com>
2021-09-18 17:57:53 +0800

    add

--- docs/react/useEnum.md
+++ docs/react/useEnum.md
@@ -7,14 +7,15 @@
  * ----------------------------------------
  * 通用枚举数据, 带缓存
  * @param {String} url - 接口地址
- * @param {String} [cached] - 是否使用缓存
+ * @param {Boolean} [cached=true] - 是否使用缓存: 如果是false,使用组件时会请求一次数据
+ * @param {Function} [handler=null] - 如果不是数组时:需要处理返回数据
  * @param {String} [id='id/_id'] - ById的键
  * ----------------------------------------
  */
 import useEnum from "@hook/useEnum";
 
 export default function useProType() {
-  const { datas, datasById } = useEnum({ url: "/api/pro/type" });
-  return { datas, datasById };
+  // 数据格式: { datas, datasById }
+  return useEnum({ url: "/api/pro/type" });
 }
 ```
--- react/useEnum.js
+++ react/useEnum.js
@@ -1,16 +1,21 @@
 import { useEffect, useState } from "react";
 import http from "@script/http";
-import byId from "ynw/array/byId";
 const cache = {};
 /**
  * ----------------------------------------
  * 通用枚举数据, 带缓存
  * @param {String} url - 接口地址
- * @param {String} [cached] - 是否使用缓存数据
- * @param {String} [id='id'] - ById的键
+ * @param {Boolean} [cached=true] - 是否使用缓存: 如果是false,使用组件时会请求一次数据
+ * @param {Function} [handler=null] - 如果不是数组时:需要处理返回数据
+ * @param {String} [id='id/_id'] - ById的键
  * ----------------------------------------
  */
-export default function useAssetsFirstType({ url, id = "id", cached = true }) {
+export default function useAssetsFirstType({
+  url,
+  id = "id",
+  cached = true,
+  handler,
+}) {
   const [datasById, setById] = useState(cache[url]?.datasById || {});
   const [datas, setdatas] = useState(cache[url]?.datas || []);
 
@@ -19,6 +24,9 @@ export default function useAssetsFirstType({ url, id = "id", cached = true }) {
       return;
     }
     http.get(url).then(res => {
+      if (handler) {
+        res = handler(res);
+      }
       const datas = res;
       const datasById = byId(res, id);
       setdatas(datas);
