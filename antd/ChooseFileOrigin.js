d5f376c7e474e75fc5ca4b72a46605210ee83f82
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-10-06 22:57:12 +0800

    add

--- antd/ChooseFileOrigin.js
+++ antd/ChooseFileOrigin.js
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import UUIDBrowser from "ynw/browser/UUIDBrowser";
 /**
  * -------------------------------------
  * 选择文件
  * @param {Function} onChange
  * @param {String} [accept] 格式过滤
+ * @param {File} [file] 如果file是null,则重置选择的文件
  * @param {Function} [as='readAsArrayBuffer'] reader的读取方法
  * -------------------------------------
  */
-export default function ChooseFileOrigin({ onChange, accept = "*" }) {
+export default function ChooseFile({ onChange, accept = "*", file }) {
   const [id] = useState(UUIDBrowser());
   const onInputChange = e => {
     const { target } = e.nativeEvent;
@@ -17,6 +18,13 @@ export default function ChooseFileOrigin({ onChange, accept = "*" }) {
       onChange(file);
     }
   };
+
+  useEffect(() => {
+    if (!file) {
+      document.getElementById(id).value = "";
+    }
+  }, [file]);
+
   return (
     <label htmlFor={id}>
       <input id={id} type="file" onChange={onInputChange} accept={accept} />
---
+++ script/numberArray.js
@@ -0,0 +1,10 @@
+/**
+ * ----------------------------------------
+ * 数字填充的数组
+ * ----------------------------------------
+ */
+function numberArray(length) {
+  return Array.from({ length }, (_, i) => i);
+}
+
+module.exports = numberArray;
