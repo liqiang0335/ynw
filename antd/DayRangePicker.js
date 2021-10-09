b7f45430b6c01b9406e354e4ffbfee61ceb8ac52
liqiang0335 <liqiang0335@qq.com>
2021-10-09 17:21:58 +0800

    add

---
+++ antd/DayRangePicker.js
@@ -0,0 +1,23 @@
+import React from "react";
+import moment from "moment";
+import { DatePicker } from "antd";
+const { RangePicker } = DatePicker;
+import { compact } from "lodash";
+/**
+ * ----------------------------------------
+ * 日期区间选择
+ * @param {Array} value - [String]
+ * @param {Function} onChange(dates) - dates: [String]
+ * ----------------------------------------
+ */
+export default function RangePickerWrap({ value, onChange }) {
+  const _onChange = (_, dateString) => {
+    onChange(dateString);
+  };
+  return (
+    <RangePicker
+      value={compact(value).map(v => moment(v))}
+      onChange={_onChange}
+    />
+  );
+}
