482ad97004943bba45c96b6a7c4ca5eb35ab01d5
liqiang0335 <liqiang0335@qq.com>
2021-10-20 18:52:35 +0800

    add

--- array/tree2Array.js
+++ array/tree2Array.js
@@ -1,21 +1,22 @@
 /**
+ * 将树结构转换为数组
  * @param {Array/Object} nodes
  * @return {Array}
  */
-export default function tree2Array(nodes) {
+export default function tree2Array(nodes, key = "children") {
   let result = [];
 
   if (Array.isArray(nodes)) {
     const len = nodes.length;
     for (var i = 0; i < len; i++) {
       result.push(nodes[i]);
-      if (nodes[i].children) {
-        result = result.concat(tree2Array(nodes[i].children));
+      if (nodes[i][key]) {
+        result = result.concat(tree2Array(nodes[i][key]));
       }
     }
   } else {
     result.push(nodes);
-    result = result.concat(tree2Array(nodes.children));
+    result = result.concat(tree2Array(nodes[key]));
   }
   return result;
 }
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import "./tiper";
+/**
+ * ----------------------------------------
+ * Array
+ * ----------------------------------------
+ */
+import { default as array2Tree } from "./array/array2Tree";
+import { default as arrayById } from "./array/arrayById";
+import { default as byId } from "./array/byId";
