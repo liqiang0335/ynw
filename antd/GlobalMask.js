b184fc1bd30412597402a2840d4d3fdedbaaceac
liqiang0335 <liqiang0335@qq.com>
2022-02-12 19:30:23 +0800

    add

--- antd/GlobalMask.js
+++ antd/GlobalMask.js
@@ -50,7 +50,7 @@ const SBox = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.75);
+  background-color: rgba(255, 255, 255, 0.9);
   display: flex;
   align-items: center;
   justify-content: center;
