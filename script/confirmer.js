ed4446b3822c8699bb7183bfbccf7ad3811e768a
liqiang0335 <liqiang0335@qq.com>
2022-02-16 16:37:33 +0800

    add

--- script/confirmer.js
+++ script/confirmer.js
@@ -5,7 +5,7 @@ import { ExclamationCircleOutlined } from "@ant-design/icons";
  * 操作确认
  * @returns {Promise}
  */
-export default function confirmer(content, title = "操作确认") {
+export default function confirmer(content = "确定要删除吗", title = "操作确认") {
   return new Promise((resolve, reject) => {
     Modal.confirm({
       icon: <ExclamationCircleOutlined style={{ color: "red" }} />,
