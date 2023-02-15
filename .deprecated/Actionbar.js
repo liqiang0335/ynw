b45e24a05e16af0bd54045922968e481e34bd478
liqiang0335 <liqiang0335@qq.com>
2023-02-15 09:36:09 +0800

    remove deprecate

--- .deprecated/Actionbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-/**
- * ----------------------------------------
- * @deprecated
- * ----------------------------------------
- */
-export default function Actionbar(props) {
-  return <div style={{ padding: "0px 0px 10px 0px", display: "flex" }} {...props} />;
-}
--- .deprecated/Spaces.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function SpaceX({ value = 10 }) {
-  return <div style={{ height: value + "px", width: "100%" }}></div>;
-}
-
-export function SpaceY({ value = 10 }) {
-  return <div style={{ width: value + "px", height: "100%" }}></div>;
-}
